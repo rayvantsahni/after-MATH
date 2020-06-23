ecc27a67615efd2c1df1024436c622a4b5dabc41
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-06-24 05:07:47 +0530

    Create GoldenRatioApproximation.java

---
+++ Approximating the Golden Ratio/GoldenRatioApproximation.java
@@ -0,0 +1,32 @@
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Scanner;
+import java.util.*;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        double goldenRatio;
+        Scanner scan = new Scanner(System.in);
+
+        ArrayList<Integer> fiboSeries = new ArrayList<Integer>();
+        fiboSeries.add(0);
+        fiboSeries.add(1);
+
+        System.out.println("Enter the value of n");
+        int n = scan.nextInt();
+
+        for (int i = 2; i <= n; i++)
+            fiboSeries.add(fiboSeries.get(i-1) + fiboSeries.get(i-2));
+
+        System.out.println("Fibonacci series till the " + (n+1) + "th term:\n" + fiboSeries);
+
+        goldenRatio = (double)fiboSeries.get(n) / fiboSeries.get(n-1);
+        System.out.println("Diving the " + (n+1) + "th term by the " + n + "th term to get an approximate of the Golden Ratio,");
+        System.out.println("i.e,\n" + fiboSeries.get(n) + " / " + fiboSeries.get(n-1));
+        System.out.println("Hence, we get: " + goldenRatio);
+
+    }
+}
