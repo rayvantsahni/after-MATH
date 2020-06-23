e48620cc811e1a1c170dfc9560e5e927d5cd5676
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-06-23 23:27:35 +0530

    Create FibonacciUsingRecursion.py

---
+++ Fibonacci Series/FibonacciUsingRecursion.py
@@ -0,0 +1,11 @@
+number = int(input("which fibonacci term do you want?\n"))
+
+def fib(n):
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    if n > 2:
+        return fib(n - 1) + fib(n - 2)
+    
+(fib(number))
