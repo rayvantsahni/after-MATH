4f23bbf65eef8d754f46d8ef3518f3acd30bedec
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-07-30 13:57:52 +0530

    Update gcd.py

--- GeeCeeDee/gcd.py
+++ GeeCeeDee/gcd.py
@@ -5,11 +5,9 @@ def gcd(a, b):
         return gcd(b, a % b)
     
     
-    
 def main():
     a = int(input("Enter 1st number "))
     b = int(input("Enter 2nd number "))
-    
     print("\nGCD of {0} and {1} is {2}.".format(a, b, gcd(a, b)))
     
 main()
