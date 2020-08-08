e6ee5c6803bd48e05167a51120b9e9e281dabb07
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-08-08 21:09:53 +0530

    Update gcd.py

--- GeeCeeDee/gcd.py
+++ GeeCeeDee/gcd.py
@@ -3,11 +3,11 @@ def gcd(a, b):
         return a
     else:
         return gcd(b, a % b)
-    
-    
+        
+
 def main():
-    a = int(input("Enter 1st number "))
-    b = int(input("Enter 2nd number "))
-    print("\nGCD of {0} and {1} is {2}.".format(a, b, gcd(a, b)))
+    from functools import reduce
+    numbers = list(map(float, input("Enter the numbers space separated\n").split()))
+    print("\nGCD:", reduce(gcd, numbers))
     
 main()
