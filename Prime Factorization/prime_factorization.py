66d26c6f55a86213ec385f11dc9694805fee34ee
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-08-23 18:25:04 +0530

    Update prime_factorization.py

--- Prime Factorization/prime_factorization.py
+++ Prime Factorization/prime_factorization.py
@@ -7,7 +7,7 @@ def prime(n):
     return True
 
 
-def prime_factor(n):
+def get_factors(n):
     divisors = []
 
     for i in range(2, math.floor(n ** .5) + 1):
@@ -17,10 +17,11 @@ def prime_factor(n):
     return list(set(divisors + [n]))
     
     
+    
 def get_prime_factors(n):
     prime_factors = []
     
-    for j in [i for i in prime_factor(n) if prime(i)]:
+    for j in [i for i in get_factors(n) if prime(i)]:
         while n != 1:
             if n % j == 0:
                 prime_factors.append(j)
@@ -29,7 +30,6 @@ def get_prime_factors(n):
                 break
                 
     return prime_factors
-
 
 
 print(get_prime_factors(660))
