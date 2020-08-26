45e698d15fa033cda88bb7b480a8b306f7c446f5
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-08-27 02:57:44 +0530

    Create IsDisarium.java

---
+++ Disarium Numbers/IsDisarium.java
@@ -0,0 +1,23 @@
+class Main 
+{
+    public static boolean IsDisarium(int m)
+    {
+        int sum = 0, n = m;
+        int length = Integer.toString(n).length();
+
+        while (n != 0)
+        {
+            sum = sum + (int) Math.pow(n % 10, length--);
+            n = n / 10;
+        }
+
+        return sum == m;
+    }
+
+
+    public static void main(String[] args) 
+    {
+        int number = 175;
+        System.out.println(IsDisarium(number) ? "Is Disarium" : "NOT Disarium");
+    }
+}
