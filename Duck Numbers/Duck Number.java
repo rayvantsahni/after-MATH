c1676870e78f37f1e42d2eebe4670f43994a5a45
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-08-27 02:07:25 +0530

    Create Duck Number.java

---
+++ Duck Numbers/Duck Number.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main 
+{
+    public static boolean IsDuckNumber(String s)
+    {
+        while (s.startsWith("0"))
+        {
+            s = s.substring(1);
+        }
+
+        return s.contains("0");
+    }
+
+
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number to check for Duck Number");
+        String number = scan.nextLine();
+        
+        System.out.println(IsDuckNumber(number) ? "Is a Duck Number" : "Is NOT a Duck Number");
+    }
+}
