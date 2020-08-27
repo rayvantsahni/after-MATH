6846251f0a630a3939d4e7d72f4c045ad0c40f68
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-08-28 02:42:51 +0530

    Create IsPetersonNumber.java

---
+++ Peterson Number/IsPetersonNumber.java
@@ -0,0 +1,31 @@
+class Main 
+{
+    public static int factorial(int n)
+    {
+        if (n == 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+
+
+    public static boolean IsPetersonNumber(int x)
+    {
+        int sum = 0, n = x;
+
+        while (n != 0)
+        {
+            sum += factorial(n % 10);
+            n /= 10;
+        }
+
+        return (sum == x);
+    }
+
+
+    public static void main(String[] args) 
+    {
+        int number = 145;
+        System.out.println(IsPetersonNumber(number) ? "Is a Peterson Number" : "Is NOT a Peterson Number");
+    }
+}
