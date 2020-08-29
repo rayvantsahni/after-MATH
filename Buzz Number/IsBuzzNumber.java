c85e1b72e4ff159c8dffd871db5438ad95a4360e
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-08-30 02:08:23 +0530

    Update IsBuzzNumber.java

--- Buzz Number/IsBuzzNumber.java
+++ Buzz Number/IsBuzzNumber.java
@@ -1,6 +1,6 @@
 class Main 
 {
-    public static boolean IsBuzzNumber(int n)
+    public static boolean isBuzzNumber(int n)
     {
         return ((n % 10 == 7) || (n % 7 == 0));
     }
@@ -9,6 +9,6 @@ class Main
     public static void main(String[] args) 
     {
         int number = 7010307;
-        System.out.println(IsBuzzNumber(number) ? "Is a Buzz Number" : "Is NOT a Buzz Number");
+        System.out.println(isBuzzNumber(number) ? "Is a Buzz Number" : "Is NOT a Buzz Number");
     }
 }
