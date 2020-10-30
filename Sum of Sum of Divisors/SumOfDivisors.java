c195c4e5697bcfd913f69184eb75275fdaa602fe
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-10-30 11:45:26 +0530

    Add files via upload

---
+++ Sum of Sum of Divisors/SumOfDivisors.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class SumOfDivisors
+{
+    public static int sumOfDivisors(int n) {
+        if (n == 1)
+            return 1;
+
+        int sum = n + 1;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+
+                if (i == n / i)
+                    continue;
+                sum += n / i;
+            }
+        }
+
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = scanner.nextInt();
+
+        long sum = 0;
+        for (int i = 1; i <= n; i++)
+            sum += sumOfDivisors(i);
+
+        System.out.println(sum);
+    }
+}
