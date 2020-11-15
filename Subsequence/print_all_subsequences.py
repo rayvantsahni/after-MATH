39de0893f85d99530defea5cb17e80628977cd0c
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-11-16 01:18:32 +0530

    Add files via upload

---
+++ Subsequence/print_all_subsequences.py
@@ -0,0 +1,21 @@
+# This algorithm uses a top-down approach of recursion
+# and becasue we only need to perform one action here
+# for each subsequence, i.e print.
+# Hence, we don't need to store the subsequences as
+# they are generated. We can simply print a subsequence
+# and move forward.
+
+
+
+def print_subsequences(string, output_subsequence = ""):
+    if not string:  # checking if there is any character left if the string(is it an empty string or not)
+        print(output_subsequence)  # printing the output after no character is left in the string to work upon
+        return
+
+    print_subsequences(string[1:], output_subsequence)  # we exclude the first character of the string at hand for the output string and generate subsequences of the rest of the string
+    print_subsequences(string[1:], output_subsequence + string[0])  # we include the first character of the string at hand for the output string and generate subsequences of the rest of the string
+
+
+if __name__ == "__main__":
+    string = input("Enter string: ")
+    print_subsequences(string)
