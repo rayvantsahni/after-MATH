dee4d704c07bf649149dda818c2ea8736702acc8
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-11-18 02:17:48 +0530

    Add files via upload

---
+++ Narcissistic Number/is_narcissistic_number.py
@@ -0,0 +1,19 @@
+def is_narcissistic_number(n):
+    original_number = n
+    number_of_digits = get_number_of_digits(n)
+    sum = 0
+
+    while n != 0:
+        sum += (n % 10) ** number_of_digits
+        n //= 10
+
+    return original_number == sum
+
+
+def get_number_of_digits(n):
+    return len(str(n))
+
+
+if __name__ == "__main__":
+    n = int(input("Enter number: "))
+    print(is_narcissistic_number(n))
